a373edfc04c3f6f8bb79b5d2f0b24ea315df5322
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-11 10:44:32 +0200

    Add boilerplate code

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_it_couchgamessoftware_helloworld_metal_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
