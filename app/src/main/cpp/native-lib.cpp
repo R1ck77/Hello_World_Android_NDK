e603e98d32ded906df85cf509c67ec9b66cbd40b
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-12 22:43:49 +0200

    Basic interfacing with Lua added
    All my problems stem from the fact that I didn't declare the lua library as 'extern "C"'

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,20 @@
 #include <jni.h>
 #include <string>
+extern "C" {
 #include <lua.h>
+#include <lualib.h>
 #include <lauxlib.h>
+}
 
-void lua_something() {
+char* lua_something() {
   lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+  lua_pushstring(L, "Hello, from Lua!");
+  const char *result = lua_tostring(L, 1);
+  lua_pop(L, 1);
+  
+  lua_close(L);
+  return strdup(result);
 }
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -12,8 +22,8 @@ Java_it_couchgamessoftware_helloworld_metal_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
 
-  lua_something();
-  
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+  char *result = lua_something();
+  jstring android_result = env->NewStringUTF(result);
+  free(result);
+  return android_result;
 }
