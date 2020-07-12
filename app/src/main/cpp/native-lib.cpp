6f28f51bb63faefcb42661b7fe7fef743f6699b4
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-12 20:42:07 +0200

    Trying to understand how cmake works
    I have some problems when linking against a library created in a subdirectory

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,19 @@
 #include <jni.h>
 #include <string>
+#include <lua.h>
+#include <lauxlib.h>
+
+void lua_something() {
+  lua_State *L = luaL_newstate();
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_it_couchgamessoftware_helloworld_metal_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+
+  lua_something();
+  
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
