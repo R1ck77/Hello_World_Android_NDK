509f34fa8db4d1d39520edaad1545f1525394913
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-17 23:25:03 +0200

    Brutal addition of libguile and friends
    The binaries are created out of the following packages:
    
        gc-7.6.12.tar
        gmp-6.2.0.tar.xz
        guile-2.2.7.tar.gz
        libatomic_ops-7.6.10.tar.gz
        libffi-3.3.tar.gz
        libiconv-1.16.tar.gz
        libtool-2.4.6.tar.gz
        libunistring-0.9.10.tar.gz
    
    unmodified save for minimal edits required to resolve missing
    symbols/configure problems.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,28 +1,16 @@
 #include <jni.h>
 #include <string>
 extern "C" {
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
+  //  #include <libguile.h>
 }
 
-char* lua_something() {
-  lua_State *L = luaL_newstate();
-  luaL_openlibs(L);
-  lua_pushstring(L, "Hello, from Lua!");
-  const char *result = lua_tostring(L, 1);
-  lua_pop(L, 1);
-  
-  lua_close(L);
-  return strdup(result);
-}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_it_couchgamessoftware_helloworld_metal_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
 
-  char *result = lua_something();
+  char *result = strdup("Just a header for now…");
   jstring android_result = env->NewStringUTF(result);
   free(result);
   return android_result;
