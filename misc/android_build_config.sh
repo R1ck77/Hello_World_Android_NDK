c45856dcc8b2ae2be0c5e93c7bccb7f0d8400f0c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-17 18:17:40 +0200

    Minor changes to a build script

--- misc/android_build_config.sh
+++ misc/android_build_config.sh
@@ -1,4 +1,5 @@
-NDK=/home/dimeo/local/android-sdk/ndk/21.0.6113669
+#NDK=/home/dimeo/local/android-sdk/ndk/21.0.6113669
+NDK=/home/dimeo/local/Android/Sdk/ndk/21.3.6528147
 
 export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
 
@@ -23,7 +24,10 @@ export STRIP=$TOOLCHAIN/bin/arm-linux-androideabi-strip
 export LDFLAGS="$LDFLAGS -L/scratch/guile_compilation/local/lib"
 export CPPFLAGS="$CPPFLAGS -I/scratch/guile_compilation/local/include"
 
+export GUILE_FOR_BUILD=/scratch/guile_compilation/guile_for_build/bin/guile
+
+# This one is probably a disaster:
+export LDFLAGS="-L/home/dimeo/local/Android/Sdk/ndk/21.3.6528147/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/21 -lm"
+
 echo "Execute: ./configure --host $TARGET"
-
-
 
