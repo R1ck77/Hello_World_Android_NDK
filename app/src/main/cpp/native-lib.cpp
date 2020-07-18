1df27a71f443404f8be4d8aa8a42d9c2b998a5cc
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-18 11:47:42 +0200

    Add Guile invocation. Doesn't work.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,12 +7,9 @@ Java_it_couchgamessoftware_helloworld_metal_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
 
-  void* x = (void*) scm_c_define;
-
-  char buffer[1000];
-  snprintf(buffer, 1000, "Header AND object reference (0x%lx)!", (unsigned long) x);
+  scm_init_guile();
   
-  char *result = strdup(buffer);
+  char *result = strdup("Header and Guile call");
   jstring android_result = env->NewStringUTF(result);
   free(result);
   return android_result;
