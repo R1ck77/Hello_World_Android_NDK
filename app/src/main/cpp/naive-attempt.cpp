bf269aa64de90a9bd31cecd629a090dd7d314037
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-26 14:19:41 +0200

    WIP to make a completely native application

---
+++ app/src/main/cpp/naive-attempt.cpp
@@ -0,0 +1,12 @@
+#include "naive-attempt.h"
+#include <jni.h>
+
+
+
+JNIEXPORT void ANativeActivity_onCreate(ANativeActivity *activity, void *savedState, size_t savedStateSize)
+{
+  
+}
+
+
+
---
+++ app/src/main/cpp/naive-attempt.h
@@ -0,0 +1,13 @@
+#ifndef NAIVE_ATTEMPT_H
+#define NAIVE_ATTEMPT_H
+#include <android/native_activity.h>
+
+//#include <android/native_window_jni.h>
+//#include <android/rect.h>
+//#include <android/surface_control.h>
+//#include <android/window.h>
+
+
+void onCreateFunction(ANativeActivity *activity, void *savedState, size_t savedStateSize);
+
+#endif
